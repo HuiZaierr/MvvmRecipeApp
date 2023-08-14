41656c59ee762ba16c8402d645f0b0d9ad929bba
闫长辉 <huizaierr@163.com>
2023-08-14 22:26:12 +0800

    1.添加buildSrc

---
+++ buildSrc/src/main/kotlin/Application.kt
@@ -0,0 +1,10 @@
+object Application {
+
+    const val applicationId = "com.ych.mvvmrecipeapp"
+    //android相关信息
+    const val compileSdk = 33
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ buildSrc/src/main/kotlin/Build.kt
@@ -0,0 +1,7 @@
+/**
+ * TODO：项目根目录，gradle中的Build
+ */
+object Build {
+//    const val build_gradle = "com.android.tools.build:gradle:${Versions.build_gradle}"
+//    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin_gradle_plugin}"
+}
