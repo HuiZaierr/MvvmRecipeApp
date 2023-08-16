de9b518359a9b78fb1415ec8eaeeb79574e5f57c
闫长辉 <huizaierr@163.com>
2023-08-17 00:07:31 +0800

    1.添加buildSrc

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 ### 布局：
 Colum：可让您垂直排列元素，相当于LinearLayout设置了android:orientation="vertical",但是不能滚动<br>
 Row：可让您水平排列元素，相当于LinearLayout设置了android:orientation="horizontal",但是不能滚动<br>
+Spacer：间隔符
 
 
 ### 修饰符：
@@ -23,4 +24,5 @@ matchParentSize：只能用于Spacer中，使某个子项与父项 Box 同样大
 height：设置高度<br>
 width：设置宽度<br>
 weight：设置权重，单位为float，只能用于Row中<br>
+clip(CircleShape)：将图像剪辑成圆形<br>
 clickable：设置点击<br>
--- app/src/main/java/com/ych/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/ych/mvvmrecipeapp/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.ych.mvvmrecipeapp
 
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(Modifier.fillMaxWidth()){
-//                MessageCard("Hello")
-                Text(text = "哈哈哈", modifier = Modifier.weight(1f))
-                MessageCard2(Message("Android", "Jetpack Compose"))
+            Column(modifier = Modifier.padding(10.dp)) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {
+                    Toast.makeText(this@MainActivity,"哈哈哈",Toast.LENGTH_SHORT).show()
+                }) {
+                    Text(text = "A BUTTON")
+                }
             }
         }
 
@@ -35,16 +48,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-private fun MessageCard(s: String) {
-    Text(text = "$s World~~~~")
-}
 
-@Composable
-private fun MessageCard2(message: Message) {
-    Text(text = message.author)
-    Text(text = message.body)
-}
 
-data class Message(val author: String, val body: String)
 
